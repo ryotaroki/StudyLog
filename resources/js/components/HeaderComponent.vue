dd1401dc81e2351b3873d2fcd49c72f8851f5c4b
ryota <giants.hara.love@gmail.com>
2022-12-15 13:21:17 +0900

    vue導入

---
+++ resources/js/components/HeaderComponent.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <div class="header">
+            <h3>vue-Header</h3>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    mounted(){
+        console.log('test');
+    }
+}
+</script>
--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -1,19 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
-</head>
-
-<body>
-    @foreach ($data as $item)
-        {{ $item->name }}
-    @endforeach
-    <input type="submit" class="btn btn-info" value="send">
-</body>
-
-</html>
+<meta name="csrf-token" content="{{ csrf_token() }}">
+<link rel="stylesheet" href="{{ asset('css/app.css')}}">
+<script src="{{ mix('js/app.js')}}"></script>
